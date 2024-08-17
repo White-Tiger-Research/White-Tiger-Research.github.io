bca21f19dd9120c11c84b34121a8f3e369c75788
coffee <coffee@coffee>
2024-08-17 11:34:54 +0300

    removed dependabot

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -29,7 +29,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Darkmode(),
   ],
   right: [
-    Component.Explorer(),
+    Component.DesktopOnly(Component.Explorer()),
     Component.Graph(),
     Component.TableOfContents(),
     Component.Backlinks(),
